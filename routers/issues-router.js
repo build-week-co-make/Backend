d1c8c9d8b8759e1b1fa1716e67174fa2356559e1
Derek S <dschwan21@me.com>
2019-07-29 15:28:39 -0700

    added comment router, finished more CRUD endpoints

--- routers/issues-router.js
+++ routers/issues-router.js
@@ -6,7 +6,7 @@ const restricted = require("../middleware/restricted");
 
 const router = express.Router();
 
-//GET request  list of issues created by this user
+//GET request  list of all issues
 
 router.get("/", restricted, (req, res) => {
   console.log("req.jwtToken", req.jwtToken);
@@ -17,7 +17,7 @@ router.get("/", restricted, (req, res) => {
     .catch(err => res.send(err));
 });
 
-//GET request for main feed of logged in Issues' issues
+//GET request for issue by id
 
 router.get("/:id", restricted, async (req, res) => {
   const id = req.params.id;
@@ -35,6 +35,45 @@ router.get("/:id", restricted, async (req, res) => {
   }
 });
 
+//GET Comments by issue
+
+router.get("/:id/comments", restricted, async (req, res) => {
+  const id = req.params.id;
+  console.log("req.jwtToken", req.jwtToken);
+  try {
+    const getComments = await Issues.getCommentsByIssueId(id);
+    if (getComments) {
+      res.status(200).json(getComments);
+    } else {
+      res.status(404).json({ message: "Issue with that ID does not exist" });
+    }
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ error: "We ran into an error retrieving the comments" });
+  }
+});
+
+router.get("/:id/withComments", async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const findIssue = await Issues.findById(id);
+    console.log("is the issue found:", findIssue);
+    const issueWithComments = await Issues.getIssueWithComments(id);
+    if (findIssue) {
+      res.status(200).json(issueWithComments);
+    } else {
+      res.status(404).json({
+        message: "Unable to locate comments from an issue with that id"
+      });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Error trying to get Issue Object" });
+  }
+});
+
 //ADD an Issue
 
 router.post("/", restricted, async (req, res) => {
@@ -48,13 +87,6 @@ router.post("/", restricted, async (req, res) => {
 
     res.status(500).json(error);
   }
-  // Users.add(user)
-  //   .then(saved => {
-  //     res.status(201).json(saved);
-  //   })
-  //   .catch(error => {
-  //     res.status(500).json(error);
-  //   });
 });
 
 //UPDATE an issue
@@ -63,34 +95,33 @@ router.put("/:id", restricted, (req, res) => {
   const id = req.params.id;
   console.log(id);
 
-  Users.update(id, req.body)
-    .then(user => {
-      res.status(200).json(user);
+  Issues.update(id, req.body)
+    .then(issue => {
+      res.status(200).json(issue);
     })
     .catch(error => {
       res
         .status(500)
-        .json({ message: "We ran into an error updating the user" });
+        .json({ message: "We ran into an error updating the issue" });
     });
 });
 
 //DELETE an issue
 
-router.delete("/:id", async (req, res) => {
+router.delete("/:id", restricted, async (req, res) => {
   const id = req.params.id;
   try {
-    const deleteProject = await ProjectDb.remove(id);
-    console.log(deleteProject);
-
-    if (deleteProject > 0) {
-      res.status(200).json({ message: "The project has been deleted" });
+    const deleteIssue = await Issues.remove(id);
+    console.log(deleteIssue);
+    if (deleteIssue > 0) {
+      res.status(200).json({ message: "The Issue has been deleted" });
     } else {
       res
         .status(404)
-        .json({ message: "Unable to delete the project with that id" });
+        .json({ message: "Unable to delete the Issue with that id" });
     }
   } catch (error) {
-    res.status(500).json({ error: "Error trying to delete project" });
+    res.status(500).json({ error: "Error trying to delete Issue" });
   }
 });
 
