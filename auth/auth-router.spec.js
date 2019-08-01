53c3f2d1461ad9deaff525dbe6bbe2e1db106d03
Derek S <dschwan21@me.com>
2019-08-01 10:50:38 -0700

    added categories

---
+++ auth/auth-router.spec.js
@@ -0,0 +1,37 @@
+const request = require("supertest");
+const db = require("../data/db-config");
+const server = require("../api/server");
+
+describe("server", () => {
+  it("db environment set to testing", () => {
+    expect(process.env.DB_ENV).toBe("testing");
+  });
+
+  // describe("login", () => {
+  //   it("should return 200 OK", () => {
+  //     // rest client and make a get to '/', look at the status code
+
+  //     return request(server)
+  //       .post("/api/login")
+  //       .send({
+  //         email: "bob",
+  //         password: "pw"
+  //       })
+  //       .then(res => {
+  //         expect(res.status).toBe(200);
+  //       });
+  //   });
+  //   it("should return data in JSON", () => {
+  //     return request(server)
+  //       .post("/api/login")
+  //       .send({
+  //         username: "bob",
+  //         password: "pw"
+  //       })
+  //       .then(res => {
+  //         expect(res.type).toMatch(/json/);
+  //         expect(res.type).toBe("application/json");
+  //       });
+  //   });
+  // });
+});
